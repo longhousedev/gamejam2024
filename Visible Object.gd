8c2d2734b21480547f9808bba52b88935cd54e23
George Long <george@longhouse.dev>
2024-01-10 16:41:20 +0000

    Added visible thing

---
+++ Visible Object.gd
@@ -0,0 +1,19 @@
+class_name Visible extends Node2D
+
+var is_visible:bool
+
+# Allows you to set if object is visible
+func set_visbility(new_visibility:bool):
+	is_visible = new_visibility
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass

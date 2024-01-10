3c25de32db2c23a81d9e378a99b349fa39620a19
Toby-Best <t.j.best@qmul.ac.uk>
2024-01-10 15:59:58 +0000

    Added Rotation (Q & E keys)

--- Scripts/player_movement.gd
+++ Scripts/player_movement.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var SPEED = 300.0
+@export var ROTATION_SPEED = 1.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -17,17 +17,13 @@ func _physics_process(delta):
 	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		#velocity.y = JUMP_VELOCITY
 
+	# Get rotation
+	var rotate_direction = Input.get_axis("rotate_anticlock", "rotate_clock")
+	rotation += rotate_direction * ROTATION_SPEED * delta
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var x_direction = Input.get_axis("move_left", "move_right")
-	var y_direction = Input.get_axis("move_up", "move_down")
-	if x_direction:
-		velocity.x = x_direction * SPEED
-	else:
-		velocity.x = 0
-	if y_direction:
-		velocity.y = y_direction * SPEED
-	else:
-		velocity.y = 0
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_direction * SPEED
 
 	move_and_slide()

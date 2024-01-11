0594ac4cd82bad50b916105827d244e696816f9d
Toby-Best <t.j.best@qmul.ac.uk>
2024-01-11 14:26:50 +0000

    Added a SceneManager Node
    The Main Menu also removes itself from the scene when the Play Game button is clicked

--- Scripts/UI/play_game.gd
+++ Scripts/UI/play_game.gd
@@ -1,7 +1,10 @@
 extends Node
 
 var next_scene = preload("res://Levels/TestLevel.tscn").instantiate()
+var node_var = false
 
 func _on_pressed():
-	await get_tree().create_timer(1.0).timeout
+	await get_tree().create_timer(0.5).timeout
+	get_parent().get_parent().queue_free()
 	get_tree().root.add_child(next_scene)
+	node_var = true

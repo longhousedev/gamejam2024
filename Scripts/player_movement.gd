9160ce141f8c07dd7f2e39468c201a84bdebc169
Toby-Best <t.j.best@qmul.ac.uk>
2024-01-11 17:02:36 +0000

    Player can no longer spin around whilst dead

--- Scripts/player_movement.gd
+++ Scripts/player_movement.gd
@@ -11,17 +11,17 @@ func _physics_process(delta):
 	
 	# Get rotation
 	# First check if we are using the mouse or the keyboard for rotation	
-	if Input.is_action_just_pressed("enable_mouse"):
-		mouse_enabled = !mouse_enabled
-	if mouse_enabled:
-		look_at(get_global_mouse_position())
-	else:
-		var rotate_direction = Input.get_axis("rotate_anticlock", "rotate_clock")
-		rotation += rotate_direction * ROTATION_SPEED * delta
+	if !dead:
+		if Input.is_action_just_pressed("enable_mouse"):
+			mouse_enabled = !mouse_enabled
+		if mouse_enabled:
+			look_at(get_global_mouse_position())
+		else:
+			var rotate_direction = Input.get_axis("rotate_anticlock", "rotate_clock")
+			rotation += rotate_direction * ROTATION_SPEED * delta
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	if !dead:
 		var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 		velocity = input_direction * SPEED
 	else:

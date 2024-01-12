da51bce8a76c8100947774624825dc4e84a45298
George Long <george@longhouse.dev>
2024-01-12 13:11:31 +0000

    Enemy Chases Player

--- Scripts/move_to.gd
+++ Scripts/move_to.gd
@@ -31,8 +31,10 @@ func set_movement_target(movement_target: Vector2):
 
 func _physics_process(delta):
 	
-	#if navigation_agent.is_navigation_finished():
-		#return
+	set_movement_target(player_script.global_position)
+	
+	if navigation_agent.is_navigation_finished():
+		return
 
 	var current_agent_position: Vector2 = global_position
 	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
